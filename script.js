65a61c9f8fa70ecd4735ea912a8ba34bb259d190
Elise Robley <elise.robley00@journalism.cuny.edu>
2025-05-19 21:37:01 -0400

    Final changes before the final upload
    All the news additions are functional

--- script.js
+++ script.js
@@ -58,8 +58,7 @@ const questionContainer = document.getElementById('question-container'); //Gets 
 
 function startQuiz(){ //This function is called when the user clicks the start button
     
-    // var meds = document.getElementsByClassName('medications');
-    var instructionSection = document.getElementsByClassName('instructions');
+    var instructionSection = document.getElementsByClassName('instructions'); //Gets the instructionSection div from the HTML. Puts it into an array
 
     instructionSection[0].style.display = 'none'; //Removes the instructions
     questionContainer.style.display = 'block'; //Displays the div with the question, information and input boxes
@@ -69,10 +68,13 @@ function startQuiz(){ //This function is called when the user clicks the start b
 const printMessage = document.getElementById('printMessage'); //Gets the print message element that displays the questions to the user
 let currentQuestionIndex = 0; //Sets the current question to the first medication
 
-var salePrice = medications[currentQuestionIndex].salePrice;
-var retailPrice = medications[currentQuestionIndex].retailPrice;
+var salePrice = medications[currentQuestionIndex].salePrice; //Gets the salePrice variable from the objects in the array
+var retailPrice = medications[currentQuestionIndex].retailPrice; //Gets the retailPrice variable from the objects in the array
 
 const submitButton = document.getElementById('submit'); //Gets the value of the submit button
+
+const endMessage = document.getElementById('endMessage'); //Gets the end message element that displays the final text once the quiz is complete
+const endContainer = document.getElementById('end-container'); //Gets the end container div from the HTML
 
 function readQuestions(){ //This function displays the questions for the user
 
@@ -81,11 +83,15 @@ function readQuestions(){ //This function displays the questions for the user
         printMessage.innerText = medications[currentQuestionIndex].question; //This should display the correct question that matches the index number of the medications array.
         printMessage.style.color = '#0d3b66';
         printMessage.style.fontWeight = 'bold';
-        printMessage.style.verticalAlign = 'top';
+        printMessage.style.verticalAlign = 'top'; //Displays the print message(question) above the user input boxes
     }
     else {
 
-        printMessage.innerText = "That's all. Thank you for participating in this educational game!";
+        endMessage.innerText = "You have completed the quiz. Thank you for participating in this educational game! To play again, refresh the page."; //This should display the thank you at the end
+        endMessage.style.color = '#0d3b66';
+        endMessage.style.fontWeight = 'bold';
+        endContainer.style.display = 'block';
+        questionContainer.style.display = 'none';
     }
 }
 
@@ -93,44 +99,48 @@ const resultMessage = document.getElementById('resultMessage'); //This is the me
 
 submitButton.addEventListener('click', calculate); //Adds the click event to the submit button
 
-const nextButton = document.getElementById('next');
+const nextButton = document.getElementById('next'); //Gets the Next button element from the HTML
 
 function calculate(){ //This function should be called when the user clicks the submit button
 
-    var userInput1 = document.getElementById('userInput1'); //This is the value of the user's guess for the sale price. Taken from the HTML
-    var userInput2 = document.getElementById('userInput2'); //This is the value of the user's guess for the average retail price. Taken from the HTML
+    var userInput1 = parseFloat(document.getElementById('userInput1').value); //This is the value of the user's guess for the sale price. Taken from the HTML
+    var userInput2 = parseFloat(document.getElementById('userInput2').value); //This is the value of the user's guess for the average retail price. Taken from the HTML
 
-    if(userInput1 < salePrice && userInput2 < retailPrice){
+    if(userInput1 < medications[currentQuestionIndex].salePrice && userInput2 < medications[currentQuestionIndex].retailPrice){ //Both guesses are lower than the actual amount
 
-        resultMessage.innerHTML = "Your guess for the wholesale acquisition cost (WAC) is lower than the actual price. The manufacturer sells this medication for $" + medications[currentQuestionIndex].salePrice + '. Your guess for the customer price is also lower than the actual price. The average retail price of this medication is $' + medications[currentQuestionIndex].retailPrice + "."
+        resultMessage.innerHTML = "Your guess for the wholesale acquisition cost (WAC) is <span style='color:red'>lower</span> than the actual price. The manufacturer sells this medication for <span style='color:red'>$" + medications[currentQuestionIndex].salePrice + '</span>. Your guess for the customer price is also <span style="color:red">lower</span> than the actual price. The average retail price of this medication is <span style="color:red">$' + medications[currentQuestionIndex].retailPrice + "</span>."
 
     }
-    else if(userInput1 < salePrice && userInput2 > retailPrice){
+    else if(userInput1 < medications[currentQuestionIndex].salePrice && userInput2 > medications[currentQuestionIndex].retailPrice){ //The WAC guess is lower while the retail guess is higher
 
-        resultMessage.innerHTML = "Your guess for the wholesale acquisition cost (WAC) is lower than the actual price. The manufacturer sells this medication for $" + medications[currentQuestionIndex].salePrice + '. Your guess for the customer price is higher than the actual price. The average retail price of this medication is $' + medications[currentQuestionIndex].retailPrice + "."
+        resultMessage.innerHTML = "Your guess for the wholesale acquisition cost (WAC) is <span style='color:red'>lower</span> than the actual price. The manufacturer sells this medication for <span style='color:red'>$" + medications[currentQuestionIndex].salePrice + '</span>. Your guess for the customer price is <span style="color:blue">higher</span> than the actual price. The average retail price of this medication is <span style="color:blue">$' + medications[currentQuestionIndex].retailPrice + "</span>."
     }
-    else if(userInput1 > salePrice && userInput2 < retailPrice){
+    else if(userInput1 > medications[currentQuestionIndex].salePrice && userInput2 < medications[currentQuestionIndex].retailPrice){ //The WAC guess is higher while the retail guess is lower
 
-        resultMessage.innerHTML = "Your guess for the wholesale acquisition cost (WAC) is higher than the actual price. The manufacturer sells this medication for $" + medications[currentQuestionIndex].salePrice + '. Your guess for the customer price is lower than the actual price. The average retail price of this medication is $' + medications[currentQuestionIndex].retailPrice + "."
+        resultMessage.innerHTML = "Your guess for the wholesale acquisition cost (WAC) is <span style='color:blue'>higher</span> than the actual price. The manufacturer sells this medication for <span style='color:blue'>$" + medications[currentQuestionIndex].salePrice + '</span>. Your guess for the customer price is <span style="color:red">lower</span> than the actual price. The average retail price of this medication is <span style="color:red">$' + medications[currentQuestionIndex].retailPrice + "</span>."
     }
-    else if(userInput1 > salePrice && userInput2 > retailPrice){
+    else if(userInput1 > medications[currentQuestionIndex].salePrice && userInput2 > medications[currentQuestionIndex].retailPrice){ //Both guesses are higher than the actual amount
 
-        resultMessage.innerHTML = "Your guess for the wholesale acquisition cost (WAC) is higher than the actual price. The manufacturer sells this medication for $" + medications[currentQuestionIndex].salePrice + '. Your guess for the customer price is also higher than the actual price. The average retail price of this medication is $' + medications[currentQuestionIndex].retailPrice + "."
+        resultMessage.innerHTML = "Your guess for the wholesale acquisition cost (WAC) is <span style='color:blue'>higher</span> than the actual price. The manufacturer sells this medication for <span style='color:blue'>$" + medications[currentQuestionIndex].salePrice + '</span>. Your guess for the customer price is also <span style="color:blue">higher</span> than the actual price. The average retail price of this medication is <span style="color:blue">$' + medications[currentQuestionIndex].retailPrice + "</span>."
     }
 
     nextButton.style.display = 'block';
-
-    // currentQuestionIndex++;
-    // readQuestions();
+    resultMessage.style.display = 'block'; //This should display both buttons when the result message is displayed
 
 }
 
-nextButton.addEventListener('click', nextQuestion);
+nextButton.addEventListener('click', nextQuestion); //Adds the click function to the next button
 
-function nextQuestion(){
+function nextQuestion(){ //This function is called when the user clicks the next button
+
+    document.getElementById('userInput1').value = ''; 
+    document.getElementById('userInput2').value = ''; //These statements should reset both of the user input values for each question
 
     currentQuestionIndex++;
     readQuestions();
+
+    nextButton.style.display = 'none';
+    resultMessage.style.display = 'none'; //These statements should remove the next button and the result message when each new question is displayed
 }
 
 readQuestions();
